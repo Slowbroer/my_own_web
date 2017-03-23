30d88a92d432762789725e6c34aa2ae49194f993
Slowbro <1573975217@qq.com>
2017-03-23 19:39:45 +0800

    update gitignore

---
+++ frontend/function/common.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Slowbro
+ * Date: 17/3/13
+ * Time: 下午5:19
+ */
+
+function rand_code($code_length = 6)
+{
+    if($code_length < 1)
+    {
+        $code_length = 6;
+    }
+
+    $min = 1;
+    for ($i=1;$i<$code_length;$i++)
+    {
+        $min = $min*10;
+    }
+
+
+    $max = $min*10 - 1;
+    $code = rand($min,$max);
+    return $code;
+}
+
+
+?>
