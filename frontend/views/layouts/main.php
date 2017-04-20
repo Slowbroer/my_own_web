5b2348a925bdfc2a7302cd52c86b808818addd87
Slowbro <1573975217@qq.com>
2017-04-20 15:41:25 +0800

    1.首页样式

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -75,13 +75,13 @@ AppAsset::register($this);
     </div>
 </div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; Slowbro  <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
+<!--<footer class="footer">-->
+<!--    <div class="container">-->
+<!--        <p class="pull-left">&copy; Slowbro  --><?//= date('Y') ?><!--</p>-->
+<!---->
+<!--        <p class="pull-right">--><?//= Yii::powered() ?><!--</p>-->
+<!--    </div>-->
+<!--</footer>-->
 
 <?php $this->endBody() ?>
 </body>
