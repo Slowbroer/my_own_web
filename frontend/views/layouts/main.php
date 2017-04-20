8c2f269a66b5589997bdea165703c90d784ab1a9
Slowbro <1573975217@qq.com>
2017-04-20 15:40:25 +0800

    1.首页样式

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -63,7 +63,7 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
-    <div class="container">
+    <div class="container" style="height: 280px;">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
